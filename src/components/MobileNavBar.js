e8c51c46e1969eea2e069236ac1393703a54af06
oluwatobisimi <bamitobs@gmail.com>
2022-10-26 07:26:57 +0100

    second push

--- src/components/MobileNavBar.js
+++ src/components/MobileNavBar.js
@@ -11,29 +11,29 @@ const MobileNavBar = () => {
   return (
     <>
       <nav className="flex justify-between  p-4 py-6 sticky top-0 z-[99999] bg-white lg:hidden items-center w-screen h-[12vh] overflow-hidden">
-        <div/>
-          <img src={logo} alt=""  className="pt-4 "/>
+        <div />
+        <img src={logo} alt="" className="pt-4 " />
         {nav ? <HiX
           className="text-2xl self-end"
           onClick={() => {
             toggleNav();
           }}
-        />:<HiMenu
-        className="text-xl"
-        onClick={() => {
-          toggleNav();
-        }}
-      />}
-        
+        /> : <HiMenu
+          className="text-xl"
+          onClick={() => {
+            toggleNav();
+          }}
+        />}
+
       </nav>
 
       {nav && <div className="fixed top-[12vh] z-[100000] h-[70vh] bg-white flex flex-col w-full text-2xl p-4 md:p-10 py-10  opacity-100 backdrop-blur-md font-Montaga uppercase gap-10 ">
-        
-        <a  href="#programme" target="_blank" rel="noopener noreferrer">Itinerary</a>
-        <a  href="#our-story" target="_blank" rel="noopener noreferrer">our story</a>
-         <a href="#rsvp" target="_blank" rel="noopener noreferrer">RSVP</a>
-        <a href="#gallery" target="_blank" rel="noopener noreferrer">Gallery</a>
-        <a href="#gifts" target="_blank" rel="noopener noreferrer">GIFTS</a>
+
+        <a href="#programme" >Itinerary</a>
+        <a href="#our-story" >our story</a>
+        <a href="#rsvp" >RSVP</a>
+        <a href="#gallery" >Gallery</a>
+        <a href="#gifts" >GIFTS</a>
       </div>}
     </>
   );
